159363a68efb547e87902edd2de2b2865e783c75
HP <HP@HP-Bilgisayar>
2017-07-24 21:46:24 +0300

    Hello gonder

---
+++ doganworkspace/src/com/dogan/ilbars/git2/Hello.java
@@ -0,0 +1,11 @@
+package com.dogan.ilbars.git2;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hello!!!");
+		
+	}
+	
+}
---
+++ doganworkspace/src/com/dogan/ilbars/git2/MerhabaDeneme.java
@@ -0,0 +1,10 @@
+package com.dogan.ilbars.git2;
+
+public class MerhabaDeneme {
+
+	public static void main(String[] args) {
+		System.out.println("2. deneme");
+
+	}
+
+}
